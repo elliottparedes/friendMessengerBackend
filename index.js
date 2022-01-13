71a14407b8762cb3454adcd35f347756a4bc9e25
elliottparedes <62229424+elliottparedes@users.noreply.github.com>
2022-01-12 18:28:19 -0600

    added CORS to project

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology:true})
 
 app.use(express.static('public'));
 
-
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 
 var messageRoutes = require("./routes/messageRoutes");
