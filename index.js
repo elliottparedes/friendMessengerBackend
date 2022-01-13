e122da507a434281a27fed666e5f9527cbd7b4d5
elliottparedes <62229424+elliottparedes@users.noreply.github.com>
2022-01-12 18:36:23 -0600

    applying fixes to CORS

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ const corsOptions ={
 }
 app.use(cors(corsOptions));
 
+app.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+     });
+
 
 var messageRoutes = require("./routes/messageRoutes");
 app.use(messageRoutes);
