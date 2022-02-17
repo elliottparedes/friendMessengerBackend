e5147f038420166aefb58c803abc81dcf9b6f908
elliottparedes <62229424+elliottparedes@users.noreply.github.com>
2022-02-16 20:07:09 -0600

    changing netlify app origin spelling

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.static('public'));
 
 const cors = require('cors');
 const corsOptions ={
-    origin:'https://friendmessenger.netlify.app/', 
+    origin:'https://friendmessenger.netlify.app', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
