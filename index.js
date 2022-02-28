f51a66ee944822208ec640e9bbb2862c2461a88d
elliottparedes <62229424+elliottparedes@users.noreply.github.com>
2022-02-27 23:21:19 -0600

    added origin for netflify front end

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.static('public'));
 
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://sleepy-cori-8f1d07.netlify.app', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
